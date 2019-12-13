7bc54f463100662823b461a56715e0b5b62db14a
ZG <zackgh@msn.com>
2019-12-13 14:26:30 -0700

    Create inverter.v

---
+++ inverter.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/11/2019 11:00:40 AM
+// Design Name: 
+// Module Name: inverter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module inverter(input a,output y);
+    assign y = ~a;
+endmodule
