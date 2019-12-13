79cd9b67baed84674ba388ca781cd4eeb8ea66a9
ZG <zackgh@msn.com>
2019-12-13 14:27:01 -0700

    Create AndGate.v

---
+++ AndGate.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/09/2019 10:22:21 AM
+// Design Name: 
+// Module Name: AndGate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module AndGate(input a,b, output y);
+    assign y = a&b;
+endmodule
