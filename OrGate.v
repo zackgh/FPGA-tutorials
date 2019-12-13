1e3fdb82850cfae21cedba27ff46cb5419428519
ZG <zackgh@msn.com>
2019-12-13 14:27:34 -0700

    Create OrGate.v

---
+++ OrGate.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/11/2019 10:38:52 AM
+// Design Name: 
+// Module Name: OrGate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module OrGate(input a,b,output y);
+    assign y = a|b;
+endmodule
