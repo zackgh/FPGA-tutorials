bbce03b3c82d17f39a18f90dbec074eae40c9d63
ZG <zackgh@msn.com>
2019-12-13 14:28:04 -0700

    Create XorGate.v

---
+++ XorGate.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/11/2019 10:53:24 AM
+// Design Name: 
+// Module Name: XorGate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module XorGate(input a,b,output y);
+    assign y = a^b;
+endmodule
